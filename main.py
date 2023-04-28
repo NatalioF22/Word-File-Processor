524d34c1b0a1299bae340c3c1959f40bfaf029c2
NatalioF22 <nataliogomes707@gmail.com>
2023-04-27 23:39:48 -0400

    update main.py and added a new python class

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-from docxtpl import DocxTemplate
-from datetime import datetime
-doc = DocxTemplate("original.docx")
+from Window import Window
+from tkinter import *
 
-date = datetime.today().strftime("%b %d, %Y")
-recruiter_name = "Dr. Ed Murphy"
-recruiter_job_title = "Supervisor"
-website_name = "Habndshake"
-company_name ="Tesla"
-company_address = "123 main st"
-company_city = "Boston"
-company_state = "MA"
-company_zipcode= "12345"
-job_title ="Sofwtare Developer"
-job_type = "internship"
-
-replacements = {"date":date,
-                "recruiter_name":recruiter_name,
-                "recruiter_job_title":recruiter_job_title,
-                "company_name": company_name,
-                "website_name":website_name,
-               "company_zipcode":company_zipcode,
-                "company_city": company_city,
-               "company_address":company_address,
-                "company_state":company_state,
-               "job_type":job_type,
-               "job_title":job_title}
-
-doc.render(replacements)
-doc.save("generated.docx")
+app = Tk()
+obj = Window(app)
+obj.run()
